8fd9fa4af9a5aa36d43073d4a1473f56f4a2a196
isabeela <isabeelabg@gmail.com>
2021-09-10 21:30:16 -0300

    3 files modified

--- src/app/disciplina/disciplina.component.ts
+++ src/app/disciplina/disciplina.component.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./disciplina.component.css']
 })
 export class DisciplinaComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/manutencao.service.ts
+++ src/manutencao.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
 
-@Injectable()
-export class ManutencaoService {
-
-  constructor() { }
-
+interface Manutencao {
+  id: number;
+  nome: String;
+  p1: number;
+  p2: number;
+  diaSemana: number;
+  horario: number;
 }
+
+@Injectable()
+export class ManutencaoService {}
